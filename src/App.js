0743cb7802033877a288cdd7182a7598c4692edb
Ankit <ankitkumargupta00@gmail.com>
2020-11-26 13:03:05 +0530

    added counter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [timer, setTimer] = useState(0);
+  const [isActive, setActive] = useState(false);
+  const [btnText, setbtnText] = useState("Start");
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (isActive) {
+        setTimer(timer + 1);
+      }
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [timer, isActive]);
+
+  const startTimer = () => {
+    if (timer === 0) setTimer(1);
+    if (isActive) {
+      setbtnText("Start");
+    } else {
+      setbtnText("Stop");
+    }
+    setActive(!isActive);
+  };
+
+  const reset = () => {
+    setTimer(0);
+    setActive(false);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{timer}</h1>
+      <span
+        className="start action-button shadow animate blue"
+        onClick={startTimer}
+      >
+        {btnText}
+      </span>
+      <span className="reset action-button shadow animate red" onClick={reset}>
+        Reset
+      </span>
     </div>
   );
 }
